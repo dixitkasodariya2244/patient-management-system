e0aaac5655ee45e5db88cdd3ef39a5e388d00889
dixitkasodariya2244 <dixitkasodariya2244@gmail.com>
2025-09-27 21:43:14 +0100

    feat(patient-service): add unique email address function

--- patient-service/src/main/java/org/pm/patientservice/exception/GlobalHandlerexception.java
+++ patient-service/src/main/java/org/pm/patientservice/exception/GlobalHandlerexception.java
@@ -1,5 +1,6 @@
 package org.pm.patientservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalHandlerexception {
 
@@ -15,6 +16,14 @@ public class GlobalHandlerexception {
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleExmailAlreadyExistsException(EmailAlreadyExistsException ex){
+        log.warn("Email address already exist {}", ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message: ", "Email address already exists");
         return ResponseEntity.badRequest().body(errors);
     }
 }
--- patient-service/src/main/java/org/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/org/pm/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package org.pm.patientservice.service;
 
 import org.pm.patientservice.dto.PatientRequestDTO;
 import org.pm.patientservice.dto.PatientResponseDTO;
+import org.pm.patientservice.exception.EmailAlreadyExistsException;
 import org.pm.patientservice.mapper.PatientMapper;
 import org.pm.patientservice.model.Patient;
 import org.pm.patientservice.repository.PatientRepository;
@@ -26,6 +27,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with this email already exists " + patientRequestDTO.getEmail());
+        }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
 
